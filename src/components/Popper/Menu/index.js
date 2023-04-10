4946440d03341d72d37e74964548a878380bd648
Your Name <you@example.com>
2023-04-10 14:58:27 +0700

    fix bug 2

--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -10,7 +10,7 @@ import { useState } from 'react';
 const cx = classNames.bind(styles);
 const defaultFn = () => {}
 
-function Menu({ children, items = [], onChange = defaultFn }) {
+function Menu({ children, items = [], onChange = defaultFn, hideOnClick=false}) {
     const [history, setHistory] = useState([{ data: items }]);
     const current = history[history.length - 1];
 
@@ -37,6 +37,7 @@ function Menu({ children, items = [], onChange = defaultFn }) {
     return (
         <Tippy
             interactive={true}
+            hideOnClick=  {hideOnClick}
             delay={[0, 700]}
             offset = {[12, 8]}
             placement="bottom-end"
